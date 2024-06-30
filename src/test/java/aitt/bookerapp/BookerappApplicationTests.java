c902be45f71811d2d6b5ed0c7d5439ed896ea362
domindominik <dominwro@gmail.com>
2024-06-30 14:55:11 +0200

    added java spring boot files

---
+++ src/test/java/aitt/bookerapp/BookerappApplicationTests.java
@@ -0,0 +1,13 @@
+package aitt.bookerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookerappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
